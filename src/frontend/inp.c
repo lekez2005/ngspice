0423154e57524f362662db868037e1c08275aead
Holger Vogt <holger.vogt@uni-due.de>
2018-01-05 17:36:40 +0100

    move skipping white spaces to fcn create_circbyline() now '  .end' is acknowledged

--- src/frontend/inp.c
+++ src/frontend/inp.c
@@ -1310,6 +1310,9 @@ create_circbyline(char *line)
     FILE *fp = NULL;
     if (!circarray)
         circarray = TMALLOC(char*, memlen);
+    char *p = skip_ws(line);
+    if (line < p)
+        memmove(line, p, strlen(p) + 1);
     circarray[linec++] = line;
     if (linec < memlen) {
         if (ciprefix(".end", line) && (line[4] == '\0' || isspace_c(line[4]))) {
