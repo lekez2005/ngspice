b75c387365b289ebb78d88410782e50e71a6fa86
Holger Vogt <holger.vogt@uni-due.de>
2018-01-19 00:05:30 +0100

    add predefined parameters TEMP, VT after each subckt call FIXME: This should not be necessary if we had a better sense of hierarchy during the evaluation of TEMPER

--- src/frontend/inpcom.c
+++ src/frontend/inpcom.c
@@ -6285,5 +6285,19 @@ pspice_compat(struct card *oldcard)
     new_str = copy(".model res r");
     nextcard = insert_new_line(nextcard, new_str, 10, 0);
     nextcard->nextcard = oldcard;
+
+    /* add predefined parameters TEMP, VT after each subckt call */
+    /* FIXME: This should not be necessary if we had a better sense of hierarchy
+    during the evaluation of TEMPER */
+    for (card = newcard; card; card = card->nextcard) {
+        char *cut_line = card->line;
+        if (ciprefix(".subckt", cut_line)) {
+            new_str = copy(".param temp = 'temper - 273.15'");
+            nextcard = insert_new_line(card, new_str, 0, 0);
+            new_str = copy(".param vt = 'temper * 8.6173303e-5'");
+            nextcard = insert_new_line(nextcard, new_str, 1, 0);
+        }
+    }
+
     return newcard;
 }
