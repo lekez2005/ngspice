15a5b072578ff3aa4d275310f07d37dc2d3d1050
Holger Vogt <holger.vogt@uni-due.de>
2018-04-01 22:46:39 +0200

    copied from MOS1 MOS1 replaced by VDMOS

---
+++ src/spicelib/devices/vdmos/vdmosdest.c
@@ -0,0 +1,14 @@
+/**********
+Copyright 1990 Regents of the University of California.  All rights reserved.
+Author: 1985 Thomas L. Quarles
+**********/
+
+#include "ngspice/ngspice.h"
+#include "vdmosdefs.h"
+#include "ngspice/suffix.h"
+
+
+void
+VDMOSdestroy(void)
+{
+}
