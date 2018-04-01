b9b44b4afae513d18399e908e745567fa8d0cf6b
Holger Vogt <holger.vogt@uni-due.de>
2018-04-02 00:08:15 +0200

    add type checking for vdmos

--- src/spicelib/parser/inpdomod.c
+++ src/spicelib/parser/inpdomod.c
@@ -212,6 +212,16 @@ char *INPdomodel(CKTcircuit *ckt, struct card *image, INPtables * tab)
 			INPmakeMod(modname, type, image);
     }
 
+    /*  ------  Check if model is a VDMOS FET ------- */
+    else if (strcmp(type_name, "vdmos") == 0) {
+        type = INPtypelook("VDMOS");
+        if (type < 0) {
+            err =
+                INPmkTemp
+                ("Device type VDMOS not available in this binary\n");
+        }
+    }
+
     /*  --------  Check if model is a MOSFET --------- */
     else if ((strcmp(type_name, "nmos") == 0)
 	       || (strcmp(type_name, "pmos") == 0)
