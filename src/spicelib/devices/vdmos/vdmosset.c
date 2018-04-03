2c5b12db8ce1a2d4b0c3e1aada7ab79634adbaa6
Holger Vogt <holger.vogt@uni-due.de>
2018-04-03 23:14:39 +0200

    default transconductance parameter kp set to 1

--- src/spicelib/devices/vdmos/vdmosset.c
+++ src/spicelib/devices/vdmos/vdmosset.c
@@ -40,7 +40,7 @@ VDMOSsetup(SMPmatrix *matrix, GENmodel *inModel, CKTcircuit *ckt,
             model->VDMOSjctSatCur = 1e-14;
         }
         if (!model->VDMOStransconductanceGiven) {
-            model->VDMOStransconductance = 2e-5;
+            model->VDMOStransconductance = 1;
         }
         if (!model->VDMOSgateSourceOverlapCapFactorGiven) {
             model->VDMOSgateSourceOverlapCapFactor = 0;
